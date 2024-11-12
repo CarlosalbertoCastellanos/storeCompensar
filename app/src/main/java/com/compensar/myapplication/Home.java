3a7867e8ae0d9f84198c1153ed2bffdbe7e94bd5
CarlosalbertoCastellanos <Ccastellanosgarzon@gmail.com>
2024-11-12 12:24:34 -0500

    change at home

--- app/src/main/java/com/compensar/myapplication/Home.java
+++ app/src/main/java/com/compensar/myapplication/Home.java
@@ -4,16 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,12 +19,13 @@ import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
-    private FloatingActionButton addNewProduct;
+    private FloatingActionButton addNewProduct, cartButton;
     private RecyclerView recyclerView;
 
     Database db;
     ArrayList<String> nameProducts, priceProducts, idProducts;
     CustomAdapterProducts customerAdapterProducts;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,30 +35,20 @@ public class Home extends AppCompatActivity {
 
         addNewProduct = findViewById(R.id.floatingActionButton);
         cartButton = findViewById(R.id.imageButtonCarShopping); // Asegúrate de que este ID coincide con tu XML
-        addNewProduct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, AddProduct.class));
-            }
+
+        addNewProduct.setOnClickListener(v -> {
+            startActivity(new Intent(Home.this, AddProduct.class));
         });
 
-        cartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Home.this, CartActivity.class);
-                startActivity(intent);
-            }
+        cartButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Home.this, CartActivity.class);
+            startActivity(intent);
         });
 
         ImageButton btnUbication = findViewById(R.id.imageButtonUbication);
-        btnUbication.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(Home.this, MapsActivity.class));
-                    }
-                }
-        );
+        btnUbication.setOnClickListener(view -> {
+            startActivity(new Intent(Home.this, MapsActivity.class));
+        });
 
         db = new Database(Home.this);
         nameProducts = new ArrayList<>();
@@ -90,9 +77,9 @@ public class Home extends AppCompatActivity {
             Toast.makeText(this, "No hay datos", Toast.LENGTH_SHORT).show();
         } else {
             while (cursor.moveToNext()){
-                String idProduct = cursor.getString(0);
-                String nameProduct = cursor.getString(1);
-                String priceProduct = cursor.getString(2);
+                String idProduct = cursor.getString(cursor.getColumnIndexOrThrow("id"));
+                String nameProduct = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                String priceProduct = cursor.getString(cursor.getColumnIndexOrThrow("price"));
                 idProducts.add(idProduct);
                 nameProducts.add(nameProduct);
                 priceProducts.add(priceProduct);
